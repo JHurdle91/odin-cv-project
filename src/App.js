a3e933dcd4d9badb26cec584c189e8f975b962a6
Jesse Hurdle <Jesse.Hurdle@gmail.com>
2021-06-03 17:07:43 -0400

    add components to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React from "react";
+import Bio from "./components/Bio";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Bio />
+        <Education />
+        <Experience />
+      </div>
+    );
+  };
 }
 
 export default App;
