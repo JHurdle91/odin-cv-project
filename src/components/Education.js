fdc902d437035fd9a95e6161a09110cd09929d17
Jesse Hurdle <Jesse.Hurdle@gmail.com>
2021-06-03 17:05:34 -0400

    add section components

---
+++ src/components/Education.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Education extends React.Component {
+  render() {
+    return(
+      <div className="Education">
+        <h2>Education</h2>
+      </div>
+    );
+  };
+}
+
+export default Education;
