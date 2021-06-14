f2d4970ad9fd988aecfddddc7b5c64caa6400332
Jesse Hurdle <Jesse.Hurdle@gmail.com>
2021-06-14 12:43:50 -0400

    convert Preview to functional component

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -6,57 +6,49 @@ import CvSidebar from "./CvSidebar";
 
 import "../../styles/Preview.css";
 
-class Preview extends React.Component {
-  constructor() {
-    super();
+const Preview = (props) => {
+  const { name, title, city, email, phone } = props.bio.fields;
+  const { degrees, jobs } = props;
 
-    this.state = {};
-  }
-
-  render() {
-    const { name, title, city, email, phone } = this.props.bio.fields;
-    const { degrees, jobs } = this.props;
-
-    return (
-      <div className="Preview">
-        <CvHeader name={name.text} title={title.text} />
-        <div className="CvMain">
-          <div className="historyContainerPreview">
-            <div className="historySectionPreview">
-              <h2>Education</h2>
-              {degrees.map((degree) => {
-                const { type, university, date } = degree.fields;
-                return (
-                  <CvHistory
-                    title={type.text}
-                    subtitle={university.text}
-                    date={date.text}
-                    key={degree.id}
-                  />
-                );
-              })}
-            </div>
-            <div className="historySectionPreview">
-              <h2>Experience</h2>
-              {jobs.map((job) => {
-                const { position, company, city, startDate, endDate } =
-                  job.fields;
-                return (
-                  <CvHistory
-                    title={position.text}
-                    subtitle={`${company.text}, ${city.text}`}
-                    date={`${startDate.text} - ${endDate.text}`}
-                    key={job.id}
-                  />
-                );
-              })}
-            </div>
+  return (
+    <div className="Preview">
+      <CvHeader name={name.text} title={title.text} />
+      <div className="CvMain">
+        <div className="historyContainerPreview">
+          <div className="historySectionPreview">
+            <h2>Education</h2>
+            {degrees.map((degree) => {
+              const { type, university, date } = degree.fields;
+              return (
+                <CvHistory
+                  title={type.text}
+                  subtitle={university.text}
+                  date={date.text}
+                  key={degree.id}
+                />
+              );
+            })}
           </div>
-          <CvSidebar city={city.text} email={email.text} phone={phone.text} />
+          <div className="historySectionPreview">
+            <h2>Experience</h2>
+            {jobs.map((job) => {
+              const { position, company, city, startDate, endDate } =
+                job.fields;
+              return (
+                <CvHistory
+                  title={position.text}
+                  subtitle={`${company.text}, ${city.text}`}
+                  date={`${startDate.text} - ${endDate.text}`}
+                  key={job.id}
+                />
+              );
+            })}
+          </div>
         </div>
+        <CvSidebar city={city.text} email={email.text} phone={phone.text} />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Preview;
