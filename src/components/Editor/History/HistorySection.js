94e1a1ce51c863b081875e60a8450df4922b72a1
Jesse Hurdle <Jesse.Hurdle@gmail.com>
2021-06-14 14:25:54 -0400

    convert HistorySection to functional component

--- src/components/Editor/History/HistorySection.js
+++ src/components/Editor/History/HistorySection.js
@@ -7,51 +7,40 @@ import HistoryItem from "./HistoryItem";
 
 const iconPlus = <FontAwesomeIcon icon={faPlus} />;
 
-class HistorySection extends React.Component {
-  constructor() {
-    super();
-
-    this.handleChange = this.handleChange.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
-    this.addItem = this.addItem.bind(this);
-  }
-
-  handleChange = (name, value, id) => {
-    const { itemsKey, onTextChange } = this.props;
+const HistorySection = (props) => {
+  const handleChange = (name, value, id) => {
+    const { itemsKey, onTextChange } = props;
     onTextChange(name, value, id, itemsKey);
   };
 
-  addItem = (e) => {
+  const addItem = (e) => {
     e.preventDefault();
-    const { itemsKey, onAddItem } = this.props;
+    const { itemsKey, onAddItem } = props;
     onAddItem(itemsKey);
   };
 
-  deleteItem = (id) => {
-    const { itemsKey, onDeleteItem } = this.props;
+  const deleteItem = (id) => {
+    const { itemsKey, onDeleteItem } = props;
     onDeleteItem(id, itemsKey);
   };
 
-  render() {
-    const { items, header } = this.props;
-
-    return (
-      <div className="HistorySection">
-        <h2>{header}</h2>
-        {items.map((item) => {
-          return (
-            <HistoryItem
-              item={item}
-              key={item.id.toString()}
-              onTextChange={this.handleChange}
-              onDeleteItem={this.deleteItem}
-            />
-          );
-        })}
-        <button onClick={this.addItem}>{iconPlus}</button>
-      </div>
-    );
-  }
-}
+  const { items, header } = props;
+  return (
+    <div className="HistorySection">
+      <h2>{header}</h2>
+      {items.map((item) => {
+        return (
+          <HistoryItem
+            item={item}
+            key={item.id.toString()}
+            onTextChange={handleChange}
+            onDeleteItem={deleteItem}
+          />
+        );
+      })}
+      <button onClick={addItem}>{iconPlus}</button>
+    </div>
+  );
+};
 
 export default HistorySection;
