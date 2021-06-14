db7b85e0ae372c825210767d09b83c69227b0443
Jesse Hurdle <Jesse.Hurdle@gmail.com>
2021-06-14 12:51:43 -0400

    convert Field to functional component

--- src/components/Editor/Field.js
+++ src/components/Editor/Field.js
@@ -1,34 +1,25 @@
 import React from "react";
 
-class Field extends React.Component {
-  constructor() {
-    super();
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange = (e) => {
+const Field = (props) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    this.props.onTextChange(name, value);
+    props.onTextChange(name, value);
   };
 
-  render() {
-    const { field, name } = this.props;
-    const { placeholder, id, text } = field;
-
-    return (
-      <div className="Field">
-        <input
-          type="text"
-          placeholder={placeholder}
-          id={id}
-          name={name}
-          value={text}
-          onChange={this.handleChange}
-        />
-      </div>
-    );
-  }
-}
+  const { field, name } = props;
+  const { placeholder, id, text } = field;
+  return (
+    <div className="Field">
+      <input
+        type="text"
+        placeholder={placeholder}
+        id={id}
+        name={name}
+        value={text}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
 
 export default Field;
